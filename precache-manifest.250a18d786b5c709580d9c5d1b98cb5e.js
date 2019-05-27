0bdf81c88e9c215d489255a0182bd8cbc3c4f957
Alexander <agvolkov4@yandex.ru>
2019-05-27 16:21:35 +0300

    Updates

---
+++ precache-manifest.250a18d786b5c709580d9c5d1b98cb5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0fe46e7740d02bd358ca3fddd5788f",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "fe928c5a300000d7d2a6",
+    "url": "/react-shop/static/css/main.7ce6f042.chunk.css"
+  },
+  {
+    "revision": "93a23742760868006112",
+    "url": "/react-shop/static/js/2.eb873069.chunk.js"
+  },
+  {
+    "revision": "fe928c5a300000d7d2a6",
+    "url": "/react-shop/static/js/main.c491f299.chunk.js"
+  },
+  {
+    "revision": "75bd495123a48ea82bb9",
+    "url": "/react-shop/static/js/runtime~main.0bd8b961.js"
+  }
+]);
